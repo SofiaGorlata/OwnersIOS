8d56ed1b22c5e324739a4d32ff7b9d0e1b9372b2
Sofia <sofia.gorlata@gmail.com>
2019-10-12 19:25:00 +0300

    Add folders, add MainTabBarController & ViewControllersFactory

---
+++ OwnersIOS/MainNavigation/MainTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.swift
+//  OwnersIOS
+//
+//  Created by Sofia on 12.10.2019.
+//  Copyright © 2019 Sofia. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+}
