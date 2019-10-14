da69cffe7be16587f6d2e6e6ae99e8237589518b
Sofia <sofia.gorlata@gmail.com>
2019-10-14 11:12:11 +0300

    Add tabbar items

--- OwnersIOS/MainNavigation/MainTabBarController.swift
+++ OwnersIOS/MainNavigation/MainTabBarController.swift
@@ -10,4 +10,39 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
+    private var tabBarList: [UIViewController]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        selectedIndex = 0
+        
+        setupTabBarItems()
+    }
+    
+    private func setupTabBarItems() {
+        tabBar.backgroundColor = .white
+        
+        let feedViewController = UIViewController()
+        let createPostViewController = UIViewController()
+        let servicesViewController = UIViewController()
+        
+        tabBarList = [feedViewController, createPostViewController, servicesViewController]
+
+        viewControllers = tabBarList
+        
+        let feedItem = tabBar.items?[0]
+        feedItem?.selectedImage = UIImage(named: "baseline_house_white")?.tint(with: .blue)
+        feedItem?.image = UIImage(named: "baseline_house_white")?.tint(with: .lightGray)
+        //feedItem?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        let addPostItem = tabBar.items?[1]
+        addPostItem?.selectedImage = UIImage(named: "add_post")?.tint(with: .blue)
+        addPostItem?.image = UIImage(named: "add_post")?.tint(with: .lightGray)
+        addPostItem?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        let services = tabBar.items?[2]
+        services?.selectedImage = UIImage(named: "services")?.tint(with: .blue)
+        services?.image = UIImage(named: "services")?.tint(with: .lightGray)
+        services?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+    }
 }
