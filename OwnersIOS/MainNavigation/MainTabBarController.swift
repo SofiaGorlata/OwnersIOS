66e1f325803e224930135db13caf7376de974057
Sofia <sofia.gorlata@gmail.com>
2019-10-15 19:49:24 +0300

    Use new colors & icons

--- OwnersIOS/MainNavigation/MainTabBarController.swift
+++ OwnersIOS/MainNavigation/MainTabBarController.swift
@@ -23,26 +23,29 @@ class MainTabBarController: UITabBarController {
         tabBar.backgroundColor = .white
         
         let feedViewController = UIViewController()
+        
         let createPostViewController = UIViewController()
+        
         let servicesViewController = UIViewController()
+        
         
         tabBarList = [feedViewController, createPostViewController, servicesViewController]
 
         viewControllers = tabBarList
         
         let feedItem = tabBar.items?[0]
-        feedItem?.selectedImage = UIImage(named: "baseline_house_white")?.tint(with: .blue)
-        feedItem?.image = UIImage(named: "baseline_house_white")?.tint(with: .lightGray)
-        //feedItem?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        feedItem?.selectedImage = UIImage(named: "baseline_house_white")?.tint(with: UIColor.purpleBlue)
+        feedItem?.image = UIImage(named: "baseline_house_white")?.tint(with: UIColor.lightPurple)
+        feedItem?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: 0, right: 0)
         
         let addPostItem = tabBar.items?[1]
-        addPostItem?.selectedImage = UIImage(named: "add_post")?.tint(with: .blue)
-        addPostItem?.image = UIImage(named: "add_post")?.tint(with: .lightGray)
-        addPostItem?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        addPostItem?.selectedImage = UIImage(named: "baseline_add_box_white")?.tint(with: UIColor.purpleBlue)
+        addPostItem?.image = UIImage(named: "baseline_add_box_white")?.tint(with: UIColor.lightPurple)
+        addPostItem?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: 0, right: 0)
         
         let services = tabBar.items?[2]
-        services?.selectedImage = UIImage(named: "services")?.tint(with: .blue)
-        services?.image = UIImage(named: "services")?.tint(with: .lightGray)
-        services?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        services?.selectedImage = UIImage(named: "baseline_build_white")?.tint(with: UIColor.purpleBlue)
+        services?.image = UIImage(named: "baseline_build_white")?.tint(with: UIColor.lightPurple)
+        services?.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: 0, right: 0)
     }
 }
