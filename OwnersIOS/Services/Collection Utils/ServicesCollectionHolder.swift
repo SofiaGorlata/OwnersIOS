57639b0db25e22e5fa9ab457d4a946c29bfa156d
Sofia <sofia.gorlata@gmail.com>
2019-12-15 17:54:34 +0200

    Change assets

---
+++ OwnersIOS/Services/Collection Utils/ServicesCollectionHolder.swift
@@ -0,0 +1,9 @@
+//
+//  ServicesCollectionHolder.swift
+//  OwnersIOS
+//
+//  Created by Sofia on 15.12.2019.
+//  Copyright © 2019 Sofia. All rights reserved.
+//
+
+import Foundation
---
+++ OwnersIOS/Services/ViewControllerBuilder/ServicesViewControllerBuilder.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesViewController.swift
+//  OwnersIOS
+//
+//  Created by Sofia on 15.12.2019.
+//  Copyright © 2019 Sofia. All rights reserved.
+//
+
+import Foundation
+
+class ServicesViewControllerBuilder {
+    
+    class func build() -> ServicesViewController {
+        let viewController = ServicesViewController()
+        return viewController
+    }
+}
